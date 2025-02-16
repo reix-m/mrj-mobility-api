c864c1c2d4e09c4dfcc222a0111e9669cff06b94
Maycon Jesus <maycon.r.dejesus@gmail.com>
2025-02-16 19:14:19 -0300

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['ts', 'js'],
+  testRegex: '.*.spec.ts$',
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  coverageProvider: 'v8',
+  rootDir: '.',
+  collectCoverageFrom: ['src/**/*.ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  moduleNameMapper: {
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^@infra/(.*)$': '<rootDir>/src/infra/$1',
+    '^@application/(.*)$': '<rootDir>/src/application/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
+  setupFiles: ['./test/expose-env.ts'],
+};
+
+export default config;
---
+++ scripts/build.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+RUN_DIR=$(pwd)
+
+SCRIPT_DIR=$(
+  cd $(dirname "$0")
+  pwd
+)
+
+clear() {
+  echo "Limpando a pasta dist..."
+  rm -rf ./dist/*
+}
+
+tsc() {
+  echo "Compilando Typescript..."
+  ./node_modules/.bin/tsc -p tsconfig.build.json && tsc-alias -p tsconfig.build.json
+}
+
+copy() {
+  echo "Copiando package.json para dist..."
+  cp ./package.json ./dist/package.json
+  cp ./yarn.lock ./dist/yarn.lock
+}
+
+install() {
+  echo "Instalando dependências na pasta dist..."
+  cd ./dist
+  yarn --freeze-lock-file --production
+  cd ..
+}
+
+compile() {
+  clear
+  tsc
+  copy
+  install
+}
+
+start() {
+  cd "$SCRIPT_DIR" && cd ..
+  compile
+  cd "$RUN_DIR"
+}
+
+start
