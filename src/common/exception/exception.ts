f14bf6b8af8c9347f051614c425392dc8ddfd9f9
Maycon Jesus <maycon.r.dejesus@gmail.com>
2025-02-16 19:57:42 -0300

    feat: adds common exception

---
+++ src/common/exception/exception.ts
@@ -0,0 +1,28 @@
+import { Optional } from '@src/common/types/types';
+
+export type CreateExceptionPayload<TData> = {
+  code: number;
+  message: string;
+  data?: TData;
+};
+
+export class Exception<TData> extends Error {
+  public readonly code: number;
+
+  public readonly data: Optional<TData>;
+
+  private constructor(code: number, message: string, data?: TData) {
+    super();
+
+    this.name = this.constructor.name;
+    this.code = code;
+    this.data = data;
+    this.message = message;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  public static new<TData>(payload: CreateExceptionPayload<TData>): Exception<TData> {
+    return new Exception(payload.code, payload.message, payload.data);
+  }
+}
