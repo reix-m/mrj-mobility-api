e6f9e19f8dbf6e1941f77963641722ac72a7a389
Maycon Jesus <maycon.r.dejesus@gmail.com>
2025-02-16 20:10:44 -0300

    feat: adds database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,32 @@
+import { DatabaseConfig } from 'src/config/database.config';
+import { Global, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return {
+          name: 'default',
+          type: 'postgres',
+          host: DatabaseConfig.DbHost,
+          port: DatabaseConfig.DbPort,
+          username: DatabaseConfig.DbUsername,
+          password: DatabaseConfig.DbPassword,
+          database: DatabaseConfig.DbName,
+          logging: DatabaseConfig.DbLogEnable ? 'all' : false,
+          entities: [`${__dirname}/../../modules/**/*.entity{.ts,.js}`],
+          migrationsRun: true,
+          migrations: [`${__dirname}/migration/**/*{.ts,.js}`],
+          migrationsTransactionMode: 'all',
+        };
+      },
+      async dataSourceFactory(options) {
+        return addTransactionalDataSource(new DataSource(options as DataSourceOptions));
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
