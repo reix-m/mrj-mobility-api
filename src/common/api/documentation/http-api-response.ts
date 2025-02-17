729671fd0facb6ec9e10f1e434a379dc5ef32c19
Maycon Jesus <maycon.r.dejesus@gmail.com>
2025-02-17 08:16:46 -0300

    docs: adds common http api response

---
+++ src/common/api/documentation/http-api-response.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class HttpApiResponse {
+  @ApiProperty({ type: 'number' })
+  public code: number;
+
+  @ApiProperty({ type: 'string' })
+  public message: string;
+
+  @ApiProperty({ description: 'timestamp in ms', type: 'number' })
+  public timestamp: number;
+
+  @ApiProperty({ type: Object })
+  public data: object;
+}
