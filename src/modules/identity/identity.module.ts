d2fa530be40bd1ef617fe43e510d2e0a62d83a7a
Reix <maycon.r.dejesus@gmail.com>
2025-03-02 20:14:10 -0300

    chore: load forgot password feature in identity module

--- src/modules/identity/identity.module.ts
+++ src/modules/identity/identity.module.ts
@@ -1,3 +1,4 @@
+import { ForgotPasswordService } from '@modules/identity/core/features/forgot-password/forgot-password.service';
 import { SignInService } from '@modules/identity/core/features/sign-in/sign-in.service';
 import { SignUpService } from '@modules/identity/core/features/sign-up/sign-up.service';
 import { AuthController } from '@modules/identity/http/controller/auth.controller';
@@ -6,9 +7,10 @@ import { UserAccessRepository } from '@modules/identity/infra/persistence/reposi
 import { UserRepository } from '@modules/identity/infra/persistence/repository/user.repository';
 import { Module, Provider } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { SmtpService } from '@src/common/smtp/smtp.service';
 import { ApiServerConfig } from '@src/config/api-server.config';
 
-const services: Provider[] = [SignUpService, SignInService];
+const services: Provider[] = [SignUpService, SignInService, ForgotPasswordService, SmtpService];
 
 const repositories: Provider[] = [UserRepository, UserAccessRepository];
 
