701dba441e1b52b6169a77a23d58ac80a81d2710
Reix <reix-m@pm.me>
2025-04-13 19:45:18 -0300

    chore: adds api entrypoint

--- src/application/api/main.ts
+++ src/application/api/main.ts
@@ -1,59 +1,10 @@
-import { ApiModule } from '@application/api/api.module';
-import { ApiServerConfig } from '@infra/config/api-server.config';
-import { ConsoleLogger, Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
-import { initializeTransactionalContext, StorageDriver } from 'typeorm-transactional';
+import { ServerApplication } from '@application/api/server-application';
 
-export class ServerApplication {
-  private readonly host: string = ApiServerConfig.Host;
+(async (): Promise<void> => {
+  await runApplication();
+})();
 
-  private readonly port: number = ApiServerConfig.Port;
-
-  public async run(): Promise<void> {
-    this.startTransactionalContext();
-
-    const app: NestExpressApplication = await NestFactory.create(ApiModule, {
-      logger: new ConsoleLogger({
-        json: true,
-      }),
-    });
-
-    this.buildAPIDocumentation(app);
-    this.log();
-
-    await app.listen(this.port, this.host);
-  }
-
-  private startTransactionalContext(): void {
-    initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
-  }
-
-  private buildAPIDocumentation(app: NestExpressApplication): void {
-    const title: string = 'Mrj Mobility Http Rest API';
-    const description: string = 'Mrj Mobility Http Rest API documentation';
-    const version: string = process.env.npm_package_version ?? '1.0.0';
-
-    const options: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
-      .setTitle(title)
-      .setDescription(description)
-      .setVersion(version)
-      .addBearerAuth()
-      .build();
-
-    const document: OpenAPIObject = SwaggerModule.createDocument(app, options);
-
-    SwaggerModule.setup('docs', app, document, {
-      jsonDocumentUrl: '/docs/format.json',
-    });
-  }
-
-  private log(): void {
-    Logger.log(`Server started on host: ${this.host}; port: ${this.port};`, ServerApplication.name);
-  }
-
-  public static new(): ServerApplication {
-    return new ServerApplication();
-  }
+async function runApplication(): Promise<void> {
+  const serverApplication: ServerApplication = ServerApplication.new();
+  await serverApplication.run();
 }
